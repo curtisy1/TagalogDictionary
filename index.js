2a7575da09764640295d71139907deca063db1b2
curtisy <lausklaus44@gmail.com>
2019-11-14 23:20:57 +0100

    Add both Providers for completeness

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { AppRegistry } from "react-native";
 import { ThemeProvider } from "react-native-elements";
+import { Provider as PaperProvider } from "react-native-paper";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 import "reflect-metadata";
@@ -8,7 +9,9 @@ import "reflect-metadata";
 export default function Main() {
   return (
     <ThemeProvider>
-      <App />
+      <PaperProvider>
+        <App />
+      </PaperProvider>
     </ThemeProvider>
   );
 }
